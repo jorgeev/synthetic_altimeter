d6a7b8a370020b95f3404b9c3265c60e44f9dc6e
jorgeev <bk-201@outlook.com>
2024-09-20 15:40:44 -0400

    Parellel code, working on merge everything in a single script

--- swotMask_2016_.py
+++ swotMask_2016_.py
@@ -1,7 +1,7 @@
 import numpy as np
 import xarray as xr
 from lib.mask_functions import AltimetryMask, write_netcdf
-
+from multiprocessing import Pool, cpu_count
 
 lat = np.arange(17.5, 33.5, 2/60)
 lon = np.arange(-98.4, -73.5, 2/60)
@@ -12,7 +12,19 @@ output_dir = '/home/jevz/swot_simulator/swot_pool'
 
 dates = np.arange(np.datetime64('2016-01-01'), np.datetime64('2024-12-31'), np.timedelta64(1, 'D'))
 
-for date in dates:
-    gen =  AltimetryMask(lat, lon,)
+def process_date(date):
+    gen = AltimetryMask(lat, lon)
     gulfMask = gen.get_swot(date)
     write_netcdf(date, gulfMask, lon, lat, output_dir)
+
+# Use all available CPU cores
+# num_processes = cpu_count()
+
+# Optionally, you could set a maximum number of processes
+print("Number of cores: ", cpu_count())
+num_processes = min(cpu_count(), 26)  # Example: Use up to 16 cores
+
+# Create a pool of worker processes
+with Pool(num_processes) as pool:
+    # Map the process_date function to all dates
+    pool.map(process_date, dates)
